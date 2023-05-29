93af4cdc3ae7b4d0bf4be89b1747ca687ebccead
Ana Luisa Bandeira <filipebandeira2022@gmail.com>
2023-05-28 22:28:43 -0300

    commit

--- angular-blog/src/app/components/small-card/small-card.component.ts
+++ angular-blog/src/app/components/small-card/small-card.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SmallCardComponent implements OnInit {
 
+  @Input()
+  photoCover:string = ""
+  
+  @Input()
+  cardTitle:string = ""
+
   constructor() { }
 
   ngOnInit(): void {
